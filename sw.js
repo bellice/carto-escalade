f4f18392a29132ae3c2edf888a4f18ffc1f52fb5
bellice <bellice@tuta.io>
2026-09-05 23:59:20 +0200

    Troisieme lieu : Dentelles de Montmirail
    51 secteurs, environ 1100 voies, saisis depuis Grimpe aux Dentelles 3 (CAF
    Avignon, 2023). Page d'accueil et service worker mis a jour (liste
    manuelle par lieu). Au passage : source_id renseigne sur les voies
    d'Argol, hauteur de paroi sur 3 secteurs restants, nom de l'auteur du
    topo de Crest corrige.

--- sw.js
+++ sw.js
@@ -59,6 +59,8 @@ const PRECACHE = [
   './vallee-drome-diois/data.geojson',
   './presquile-crozon/index.html',
   './presquile-crozon/data.geojson',
+  './dentelles-montmirail/index.html',
+  './dentelles-montmirail/data.geojson',
 ];
 
 // Sans ce pré-cache, la première ouverture d'une fiche paie une latence
